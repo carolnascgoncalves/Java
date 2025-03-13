48d8a9780881cd5362360cf8e0300102e28afa25
carolina123123 <carolrsc1411@gmail.com>
2025-03-13 12:13:21 -0300

    Aula 02

---
+++ Aula 02 - Orientacao a objetos/src/Livro.java
@@ -0,0 +1,9 @@
+public class Livro {
+    String titulo;
+    String autor;
+    double preco;
+    String editoria;
+    int paginas;
+    String resumo;
+    String tipoCapa;
+}
---
+++ Aula 02 - Orientacao a objetos/src/TesteLivro.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TesteLivro {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Criar um objeto -> Instanciar
+        //Tipo nomeObjeto = new Tipo();
+
+        Livro livro = new Livro();
+        System.out.println("Nome do livro: ");
+        String nome = entrada.nextLine();
+        livro.titulo = nome;
+
+        System.out.println("Preço do livro: ");
+        double preco = entrada.nextDouble();
+        livro.preco = preco;
+
+        System.out.println("Nome do livro: "+livro.titulo+", Preço do livro: R$"+livro.preco);
+
+    }
+}
